9b7cf7a2d089a5c43e9898bf719ed4007d577c28
Birhanu <birhanumatebe12@gmail.com>
2026-02-20 19:15:07 +0300

    feat: solve swap_case

--- Swap_case.py
+++ Swap_case.py
@@ -1,1 +1,14 @@
 
+def swap_case(s):
+    character = []
+    for char in s:
+        if char == char.lower():
+            character.append(char.upper())
+        else:
+            character.append(char.lower())
+    return "".join(character)
+
+if __name__ == '__main__':
+    s = input()
+    result = swap_case(s)
+    print(result)
