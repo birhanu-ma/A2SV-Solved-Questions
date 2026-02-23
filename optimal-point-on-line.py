36569406e8a269b86aa6aec154b81802b9d1b63f
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-02-23 10:18:18 +0300

    Implement optimal point calculation
    Add function to find optimal point on a line.

--- optimal-point-on-line.py
+++ optimal-point-on-line.py
@@ -1,1 +1,11 @@
+import sys
+input = sys.stdin.readline
 
+def optimalPoint(n, list1):
+    list1.sort()
+    return list1[(n - 1) // 2]
+
+n = int(input())
+arr = list(map(int, input().split()))
+
+print(optimalPoint(n, arr))
