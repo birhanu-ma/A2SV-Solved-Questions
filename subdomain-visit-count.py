fe6e187606b2ff38b1c715fc9698f3dab53b02b5
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-02-27 11:51:01 +0300

    Create subdomain-visit-count.py

---
+++ subdomain-visit-count.py
@@ -0,0 +1,12 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        mapping = {}
+        for cpdomain in cpdomains:
+            count_str, res = cpdomain.split()
+            count = int(count_str)
+            part_of_domain = res.split(".")
+            for i in range(len(cpdomain)):
+                domain =".".join(part_of_domain[i:])
+                mapping[domain] = mapping.get(domain, 0)+count
+        response = [f"{count} {value}" for value, count in mapping.items()]
+        return response
