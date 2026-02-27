c0b220c4ab0a6c4c6740306cc5b500927415102b
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-02-27 15:29:36 +0300

    Create group-anagram.py

---
+++ group-anagram.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def groupAnagrams(self, strs):
+        ans = defaultdict(list)
+        for s in strs:
+            key = "".join(sorted(s))
+            ans[key].append(s)
+        return ans.values()
+        
