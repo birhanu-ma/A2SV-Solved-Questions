7cb223ac842de2b52da72f0fe6f3375aa7fec216
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-02-27 11:59:05 +0300

    Update subdomain-visit-count.py

--- subdomain-visit-count.py
+++ subdomain-visit-count.py
@@ -5,7 +5,7 @@ class Solution:
             count_str, res = cpdomain.split()
             count = int(count_str)
             part_of_domain = res.split(".")
-            for i in range(len(cpdomain)):
+            for i in range(len(part_of_domain)):
                 domain =".".join(part_of_domain[i:])
                 mapping[domain] = mapping.get(domain, 0)+count
         response = [f"{count} {value}" for value, count in mapping.items()]
