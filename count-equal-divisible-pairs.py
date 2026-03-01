10c09fb24505c90170657273da31209f11710c69
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-03-01 17:46:11 +0300

    Add countPairs method to count equal divisible pairs
    Implement a method to count equal divisible pairs in a list.

---
+++ count-equal-divisible-pairs.py
@@ -0,0 +1,11 @@
+class Solution:
+    def countPairs(self, nums: List[int], k: int) -> int:
+        num_pairs = 0
+        for i in range(len(nums)):
+            for j in range(i+1, len(nums)):
+                if nums[i]==nums[j] and (i*j)%k==0:
+                    num_pairs+=1
+        return num_pairs
+
+
+        
