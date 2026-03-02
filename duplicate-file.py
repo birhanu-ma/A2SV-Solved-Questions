61ddbc9471ad5286cb55e801d46546161bab5bc7
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-03-02 09:28:34 +0300

    Add findDuplicate method to identify duplicate files
    Implement a solution to find duplicate files based on content.

---
+++ duplicate-file.py
@@ -0,0 +1,18 @@
+class Solution:
+    def findDuplicate(self, paths: List[str]) -> List[List[str]]:
+        d = {}
+        for path in paths:
+            parts = path.split(" ")
+            directory = parts[0]
+
+            for file in parts[1:]:
+                name, content = file.split("(")
+                content = content[:-1]
+                full_path = directory+"/"+name
+                d.setdefault(content, []).append(full_path)
+        return [group for group in d.values() if len(group) > 1]
+        
+
+
+            
+        
