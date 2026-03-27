96497d77fb7822b5b56e02c596ff80fa33354a4c
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-03-27 21:43:23 +0300

    Create matrix-rotation.py

---
+++ matrix-rotation.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def findRotation(self, mat, target):
+        """
+        :type mat: List[List[int]]
+        :type target: List[List[int]]
+        :rtype: bool
+        """
+
+        n = len(mat)
+        for k in range(4):
+            for i in range(n // 2):
+                for j in range((n + 1) // 2):
+                    (
+                        mat[i][j],
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                    ) = (
+                        mat[n - 1 - j][i],
+                        mat[n - 1 - i][n - 1 - j],
+                        mat[j][n - 1 - i],
+                        mat[i][j],
+                    )
+
+            if mat == target:
+                return True
+        return False
