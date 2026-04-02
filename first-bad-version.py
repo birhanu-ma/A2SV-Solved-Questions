08fefbf40f21bdf369694e4d1d9d0fe5886b8e2f
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-04-02 13:59:13 +0300

    Add firstBadVersion function using binary search
    Implement binary search to find the first bad version.

---
+++ first-bad-version.py
@@ -0,0 +1,25 @@
+# The isBadVersion API is already defined for you.
+# @param version, an integer
+# @return a bool
+# def isBadVersion(version):
+
+class Solution(object):
+    def firstBadVersion(self, n):
+        """
+        :type n: int
+        :rtype: int
+        """
+        low = 1
+        high = n
+        ans = 0
+        while high>=low:
+            mid = (high+low)//2
+            if isBadVersion(mid):
+                ans = mid
+                high = mid-1
+            else:
+                low = mid+1
+        return ans
+            
+
+        
