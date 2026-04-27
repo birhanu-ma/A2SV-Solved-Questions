60afcd42caed93639e3d81d73464a85d1834399d
Birhanu Matebe <birhanumatebe12@gmail.com>
2026-04-27 12:15:16 +0300

    Add string splitting solution with backtracking
    Implement a solution to split a string into consecutive integers.

---
+++ splittingString.py
@@ -0,0 +1,24 @@
+class Solution(object):
+    def splitString(self, s):
+        """
+        :type s: str
+        :rtype: bool
+        """
+        def backtrack(index, prev):
+            if index == len(s):
+                return True
+            
+            for j in range(index, len(s)):
+                val = int(s[index:j+1])
+                if val + 1 == prev:
+                    if backtrack(j + 1, val):
+                        return True
+            
+            return False
+
+        for i in range(len(s) - 1):
+            val = int(s[:i+1])
+            if backtrack(i + 1, val):
+                return True
+        
+        return False
